1736cbcf7aaef22c55fb75e6dd507c28513e838d
Fernando Medina <fmedina771@gmail.com>
2017-07-21 15:43:30 -0400

    style changes

--- hangmanGame.js
+++ hangmanGame.js
@@ -3,60 +3,45 @@ var hangmanGame = {
 
   // Object of all words that can be chosen, along with info such as their picture and a song clip.
   wordsToPick: {
-    genesis: {
-      picture: "genesis.jpg",
-      song: "Illegal Alien",
-      preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+    eminem: {
+      picture: "eminem.jpg",
+      song: "Rap God"
     },
     madonna: {
       picture: "madonna.jpg",
-      song: "Material Girl",
-      preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+      song: "Material Girl"
     },
-    toto: {
-      picture: "toto.jpg",
-      song: "Rosanna",
-      preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+    beyonce: {
+      picture: "beyonce.jpg",
+      song: "Party"
     },
     queen: {
       picture: "queen.jpg",
-      song: "Princes of the Universe",
-      preview: "https://p.scdn.co/mp3-preview/b84f24300476f3d3f20056d2388cc51db2e3891f"
+      song: "Princes of the Universe"
     },
     u2: {
       picture: "u2.jpg",
-      song: "With or Without You",
-      preview: "https://p.scdn.co/mp3-preview/28365dff1890075c1371628371cd0e5bbff9a3a3"
-    },
-    metallica: {
-      picture: "metallica.jpg",
-      song: "Master of Puppets",
-      preview: "https://p.scdn.co/mp3-preview/60e6f8dab43f176dd9fb5e795d4e6459bad52e9e"
+      song: "With or Without You"
     },
     journey: {
       picture: "journey.jpg",
-      song: "Don't Stop Believin'",
-      preview: "https://p.scdn.co/mp3-preview/21b9abd3cd2eea634e17a917196fdd5ba2e82670"
+      song: "Don't Stop Believin'"
     },
-    inxs: {
-      picture: "inxs.jpg",
-      song: "Need You Tonight",
-      preview: "https://p.scdn.co/mp3-preview/61b17a335d5afc1c4086b1b08e2400f0da147977"
+    kanye: {
+      picture: "kanye.jpg",
+      song: "Can't Tell Me Nothing"
     },
-    poison: {
-      picture: "poison.jpg",
-      song: "Fallen Angel",
-      preview: "https://p.scdn.co/mp3-preview/0365ad1f152f1834b42b857c4625191cebf9f987"
+    diddy: {
+      picture: "diddy.jpg",
+      song: "I’ll Be Missing You"
     },
-    rush: {
-      picture: "rush.jpg",
-      song: "Limelight",
-      preview: "https://p.scdn.co/mp3-preview/154987dfb07f2fc5ed7aa4d76b80c5dc08ff4d6b"
+    rhianna: {
+      picture: "rihanna.jpg",
+      song: "Stay"
     },
-    blondie: {
-      picture: "blondie.jpg",
-      song: "Call Me",
-      preview: "https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90"
+    prince: {
+      picture: "prince.jpg",
+      song: "When Doves Cry"
     }
   },
 
@@ -229,10 +214,6 @@ var hangmanGame = {
        "<img class='band-image' src='images/" +
        this.wordsToPick[this.wordInPlay].picture + "' alt='" +
        this.wordsToPick[this.wordInPlay].song + "'>";
-
-      // Play an audio track of the band.
-      var audio = new Audio(this.wordsToPick[this.wordInPlay].preview);
-      audio.play();
 
       // return true, which will trigger the restart of our game in the updatePage function.
       return true;
